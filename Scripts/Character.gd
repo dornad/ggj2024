9462d18fbd319d2dc5ae72509436d808dece5cd7
Daniel Rodriguez <daniel@danielrodriguez.name>
2024-01-28 13:57:42 -0500

    Adding End State

--- Scripts/Character.gd
+++ Scripts/Character.gd
@@ -4,9 +4,12 @@ class_name Character
 
 @export var firstItem: global.DraggableType = global.DraggableType.NONE
 
+@export var is_guilty: bool = false
 @export var normal_voice: Resource = load("res://Assets/Audio/deep_voice_Robbing.wav")
 @export var mask_voice: Resource = load("res://Assets/Audio/deep_voice_Robbing.wav")
 @export var pain_voice: Resource = load("res://Assets/Audio/deep_voice_Robbing.wav")
+
+var is_guessing_guilty: bool
 
 func _on_button_pressed():
 	if firstItem == global.DraggableType.NONE:
@@ -18,3 +21,6 @@ func _on_button_pressed():
 func _on_button_action_2_pressed():
 	%audioPlayer.stream = pain_voice
 	%audioPlayer.play()
+
+func _on_check_button_toggled(toggled_on):
+	is_guessing_guilty = toggled_on
